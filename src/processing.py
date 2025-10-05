b6da9492fd326a6377bc9c862bd1b3ce3f9c9ecc
Stanislav Dulskiy <bplayer@mail.ru>
2025-10-05 19:31:15 +0300

    correct descriptions etc

--- src/processing.py
+++ src/processing.py
@@ -1,17 +1,17 @@
-def filter_by_state(transaction_log: list, state: str = "EXECUTED") -> list:
+def filter_by_state(banking_list: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Функция принимает список словарей и опционально значение для ключ
     Функция возвращает новый список словарей, содержащий только те словари,
     у которых ключ state  соответствует указанному значению"""
-    transaction_filter = []  # отфильтрованный список словарей
-    for transaction_item in transaction_log:
-        if transaction_item["state"] == state:
-            transaction_filter.append(transaction_item)
-    return transaction_filter
+    banking_filter: list[dict] = []  # отфильтрованный список словарей
+    for banking in banking_list:
+        if banking["state"] == state:
+            banking_filter.append(banking)
+    return banking_filter
 
 
-def sort_by_date(transaction_log: list, is_reverse: bool = True) -> list:
+def sort_by_date(banking_list: list[dict], is_reverse: bool = True) -> list[dict]:
     """Функция принимает список словарей и необязательный параметр direction,
     задающий порядок сортировки (по умолчанию — убывание).
     Функция возвращает новый список, отсортированный по дате"""
-    transaction_sorted = sorted(transaction_log, key=lambda transaction: transaction["date"], reverse=is_reverse)
-    return transaction_sorted
+    banking_sorted = sorted(banking_list, key=lambda banking: banking["date"], reverse=is_reverse)
+    return banking_sorted
