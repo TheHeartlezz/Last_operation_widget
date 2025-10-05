e11f5a9da8f5563811be15047a58115c05f8880f
Stanislav Dulskiy <bplayer@mail.ru>
2025-10-05 19:54:00 +0300

    comprehansive type description

--- src/processing.py
+++ src/processing.py
@@ -1,17 +1,17 @@
-def filter_by_state(banking_list: list[dict], state: str = "EXECUTED") -> list[dict]:
+def filter_by_state(banking_list: list[dict[str, str]], state: str = "EXECUTED") -> list[dict[str, str]]:
     """Функция принимает список словарей и опционально значение для ключ
     Функция возвращает новый список словарей, содержащий только те словари,
     у которых ключ state  соответствует указанному значению"""
-    banking_filter: list[dict] = []  # отфильтрованный список словарей
+    banking_filter = []  # отфильтрованный список словарей
     for banking in banking_list:
         if banking["state"] == state:
             banking_filter.append(banking)
     return banking_filter
 
 
-def sort_by_date(banking_list: list[dict], is_reverse: bool = True) -> list[dict]:
+def sort_by_date(banking_list: list[dict[str, str]], is_reverse: bool = True) -> list[dict[str, str]]:
     """Функция принимает список словарей и необязательный параметр direction,
     задающий порядок сортировки (по умолчанию — убывание).
     Функция возвращает новый список, отсортированный по дате"""
-    banking_sorted = sorted(banking_list, key=lambda banking: banking["date"], reverse=is_reverse)
+    banking_sorted: list[dict] = sorted(banking_list, key=lambda banking: banking["date"], reverse=is_reverse)
     return banking_sorted
