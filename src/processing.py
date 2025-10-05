12b17d5392c69ea9275b8e0b3ccaf143883a6234
Stanislav Dulskiy <bplayer@mail.ru>
2025-10-05 12:48:56 +0300

    checked all linters

--- src/processing.py
+++ src/processing.py
@@ -1,21 +1,17 @@
-
-def filter_by_state(transaction_log: list, state: str = 'EXECUTED') -> list:
+def filter_by_state(transaction_log: list, state: str = "EXECUTED") -> list:
     """Функция принимает список словарей и опционально значение для ключ
     Функция возвращает новый список словарей, содержащий только те словари,
     у которых ключ state  соответствует указанному значению"""
-    transaction_filter = [] #отфильтрованный список словарей
+    transaction_filter = []  # отфильтрованный список словарей
     for transaction_item in transaction_log:
-        if transaction_item['state'] == state:
+        if transaction_item["state"] == state:
             transaction_filter.append(transaction_item)
     return transaction_filter
-
-
-
 
 
 def sort_by_date(transaction_log: list, is_reverse: bool = True) -> list:
     """Функция принимает список словарей и необязательный параметр direction,
     задающий порядок сортировки (по умолчанию — убывание).
     Функция возвращает новый список, отсортированный по дате"""
-    transaction_sorted = sorted(transaction_log, key=lambda transaction: transaction['date'], reverse=is_reverse)
+    transaction_sorted = sorted(transaction_log, key=lambda transaction: transaction["date"], reverse=is_reverse)
     return transaction_sorted
