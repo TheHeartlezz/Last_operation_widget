9b018f0fd515b78bd8745105cc8a5578cf24912e
Stanislav Dulskiy <bplayer@mail.ru>
2025-10-05 12:21:31 +0300

    add filter_by_state

--- src/processing.py
+++ src/processing.py
@@ -1,10 +1,21 @@
 
-def filter_by_state(dict_list: list, state = 'executed') -> list:
-    pass
+def filter_by_state(transaction_log: list, state = 'EXECUTED') -> list:
+    """Функция принимает список словарей и опционально значение для ключ
+    Функция возвращает новый список словарей, содержащий только те словари,
+    у которых ключ state  соответствует указанному значению"""
+    transaction_filter = [] #отфильтрованный список словарей
+    for transaction_item in transaction_log:
+        if transaction_item['state'] == state:
+            transaction_filter.append(transaction_item)
+    return transaction_filter
 
 
 
 
 
-def sort_by_date(dict_list: list, direction = 'ascending') -> list:
+def sort_by_date(transaction_log: list, direction = 'decending') -> list:
+    """Функция принимает список словарей и необязательный параметр direction,
+    задающий порядок сортировки (по умолчанию — убывание).
+    Функция возвращает новый список, отсортированный по дате"""
+
     pass
